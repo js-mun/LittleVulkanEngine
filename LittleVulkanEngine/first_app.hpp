b92176f3fab0f2168e472bc2d06a24a726650e40
mj <farfs>
2025-08-31 10:38:33 +0900

    --- Trialgle

--- LittleVulkanEngine/first_app.hpp
+++ LittleVulkanEngine/first_app.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "lve_device.hpp"
+#include "lve_pipeline.hpp"
+#include "lve_swap_chain.hpp"
 #include "lve_window.hpp"
+
+#include <memory>
+#include <vector>
 
 namespace lve {
 class FirstApp {
@@ -8,8 +14,24 @@ class FirstApp {
         static constexpr int WIDTH = 800;
         static constexpr int HEIGHT = 600;
 
+        FirstApp();
+        ~FirstApp();
+
+        FirstApp(const FirstApp &) = delete;
+        FirstApp &operator=(const FirstApp &) = delete;
+
         void run();
     private:
+        void createPipelineLayout();
+        void createPipeline();
+        void createCommandBuffers();
+        void drawFrame();
+
         LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+        LveDevice lveDevice{lveWindow};
+        LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+        std::unique_ptr<LvePipeline> lvePipeline;
+        VkPipelineLayout pipelineLayout;
+        std::vector<VkCommandBuffer> commandBuffers;
 };
 }
--- LittleVulkanEngine/lve_window.hpp
+++ LittleVulkanEngine/lve_window.hpp
@@ -11,11 +11,13 @@ class LveWindow {
     public:
         LveWindow(int w, int h, std::string name);
         ~LveWindow();
+        
         LveWindow(const LveWindow &) = delete;
         LveWindow &operator=(const LveWindow &) = delete;
 
         bool shouldClose() { return glfwWindowShouldClose(window); }
-    
+        VkExtent2D getExtent() { return {static_cast<uint32_t>(width), static_cast<uint32_t>(height)}; }
+        void createWindowSurface(VkInstance instance, VkSurfaceKHR *surface);
     private:
         void initWindow();
         const int width;
