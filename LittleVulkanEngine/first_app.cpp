cbc83557335293c87e5f49be5d45d0a92aa8e976
mj <farfs>
2025-10-08 16:08:27 +0900

    tutorial 20

--- LittleVulkanEngine/first_app.cpp
+++ LittleVulkanEngine/first_app.cpp
@@ -28,25 +28,46 @@ struct GlobalUbo {
         -3.f, -1.f});
 };
 
-FirstApp::FirstApp() { loadGameObjects(); }
+FirstApp::FirstApp() {
+    globalPool = 
+        LveDescriptorPool::Builder(lveDevice)
+            .setMaxSets(LveSwapChain::MAX_FRAMES_IN_FLIGHT)
+            .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, LveSwapChain::MAX_FRAMES_IN_FLIGHT)
+            .build();
+    loadGameObjects(); 
+}
 
 FirstApp::~FirstApp() {}
 
 void FirstApp::run() {
-  LveBuffer globalUboBuffer{
-    lveDevice,
-    sizeof(GlobalUbo),
-    LveSwapChain::MAX_FRAMES_IN_FLIGHT,
-    VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
-    64, // fix by magic number
-    // lveDevice.properties.limits.nonCoherentAtomSize, // -> fix by chat gpt
-    // lveDevice.properties.limits.minUniformBufferOffsetAlignment, // -> Sample code // returned 4 for my pc. expected to return 16~256 
-  };
+  std::vector<std::unique_ptr<LveBuffer>> uboBuffers(LveSwapChain::MAX_FRAMES_IN_FLIGHT);
+  for (int i = 0; i < uboBuffers.size(); i++) {
+    uboBuffers[i] = std::make_unique<LveBuffer>(
+        lveDevice,
+        sizeof(GlobalUbo),
+        1,
+        VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+    uboBuffers[i]->map();
+  }
 
-  globalUboBuffer.map();
+  auto globalSetLayout =
+      LveDescriptorSetLayout::Builder(lveDevice)
+          .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT)
+          .build();
+  
+  std::vector<VkDescriptorSet> globalDescriptorSets(LveSwapChain::MAX_FRAMES_IN_FLIGHT);
+  for (int i = 0; i < globalDescriptorSets.size(); i++) {
+    auto bufferInfo = uboBuffers[i]->descriptorInfo();
+    LveDescriptorWriter(*globalSetLayout, *globalPool)
+        .writeBuffer(0, &bufferInfo)
+        .build(globalDescriptorSets[i]);
+  }
 
-  SimpleRenderSystem simpleRenderSystem{lveDevice, lveRenderer.getSwapChainRenderPass()};
+  SimpleRenderSystem simpleRenderSystem{
+        lveDevice, 
+        lveRenderer.getSwapChainRenderPass(),
+        globalSetLayout->getDescriptorSetLayout()};
   LveCamera camera{};
 
   using clock = std::chrono::high_resolution_clock;
@@ -73,14 +94,19 @@ void FirstApp::run() {
 
     if (auto commandBuffer = lveRenderer.beginFrame()) {
         int frameIndex = lveRenderer.getFrameIndex();
-        FrameInfo frameInfo{frameIndex, frameTime, commandBuffer, camera};
+        FrameInfo frameInfo{
+            frameIndex, 
+            frameTime, 
+            commandBuffer, 
+            camera,
+            globalDescriptorSets[frameIndex]};
         
         // update
         GlobalUbo ubo{};
         ubo.projectionView = camera.getProjection() * camera.getView();
-        globalUboBuffer.writeToBuffer(&ubo, frameIndex);
-        globalUboBuffer.flushIndex(frameIndex);
-        
+        uboBuffers[frameIndex]->writeToBuffer(&ubo);
+        uboBuffers[frameIndex]->flush();
+            
         // render
         lveRenderer.beginSwapChainRenderPass(commandBuffer);
         simpleRenderSystem.renderGameObjects(frameInfo, gameObjects);
@@ -105,21 +131,20 @@ void FirstApp::run() {
 }
 
 void FirstApp::loadGameObjects() {
-    std::shared_ptr<LveModel> lveModel = 
-        LveModel::createModelFromFile(lveDevice, "models/smooth_vase.obj");
+  std::shared_ptr<LveModel> lveModel =
+      LveModel::createModelFromFile(lveDevice, "models/flat_vase.obj");
+  auto flatVase = LveGameObject::createGameObject();
+  flatVase.model = lveModel;
+  flatVase.transform.translation = {-.5f, .5f, 2.5f};
+  flatVase.transform.scale = {3.f, 1.5f, 3.f};
+  gameObjects.push_back(std::move(flatVase));
 
-    auto flatVase = LveGameObject::createGameObject();
-    flatVase.model = lveModel;
-    flatVase.transform.translation = {-.5f, .5f, 2.5f};
-    flatVase.transform.scale = {3.f, 1.5f, 3.f};
-    gameObjects.push_back(std::move(flatVase));
-
-    lveModel = LveModel::createModelFromFile(lveDevice, "models/smooth_vase.obj");
-    auto smoothVase = LveGameObject::createGameObject();
-    smoothVase.model = lveModel;
-    smoothVase.transform.translation = {.5f, .5f, 2.5f};
-    smoothVase.transform.scale = {3.f, 1.5f, 3.f};
-    gameObjects.push_back(std::move(smoothVase));
+  lveModel = LveModel::createModelFromFile(lveDevice, "models/smooth_vase.obj");
+  auto smoothVase = LveGameObject::createGameObject();
+  smoothVase.model = lveModel;
+  smoothVase.transform.translation = {.5f, .5f, 2.5f};
+  smoothVase.transform.scale = {3.f, 1.5f, 3.f};
+  gameObjects.push_back(std::move(smoothVase));
 }
 
 }  // namespace lve
--- LittleVulkanEngine/shaders/simple_shader.frag
+++ LittleVulkanEngine/shaders/simple_shader.frag
@@ -1,11 +1,12 @@
 #version 450
 
 layout (location = 0) in vec3 fragColor;
+
 layout (location = 0) out vec4 outColor;
 
 layout(push_constant) uniform Push {
-  mat4 transform; // projection * view * model
-  vec3 normalMatrix;
+  mat4 modelMatrix;
+  mat4 normalMatrix;
 } push;
 
 void main() {
